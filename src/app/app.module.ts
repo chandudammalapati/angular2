716e7d5525c10877a89f014db45cd24fc685714e
chandu-PC\chandu <chandudammalapati@gmail.com>
2017-07-22 19:55:20 -0400

    adding recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { headerComponent } from './header/header.component';
+import { recipesComponent } from './recipes/recipes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    headerComponent
+    headerComponent,
+    recipesComponent
   ],
   imports: [
     FormsModule,

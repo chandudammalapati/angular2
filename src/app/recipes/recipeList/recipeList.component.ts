954a0cf3d4d7158d8de33ed324388f212da263da
chandu-PC\chandu <chandudammalapati@gmail.com>
2017-07-23 13:36:52 -0400

    creating and adding header section

--- src/app/recipes/recipeList/recipeList.component.ts
+++ src/app/recipes/recipeList/recipeList.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
     selector:'app-recipeList',
     templateUrl: 'recipeList.component.html'
 })
-export class recipeListComponent{
-    
+export class recipeListComponent implements OnInit {
+    recipes = [];
+    constructor(){
+
+    }
+    ngOnInit(){
+
+    }
 }

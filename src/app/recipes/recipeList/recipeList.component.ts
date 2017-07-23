efc5298aa00b10925617dedcdaf51e7e44c36f92
chandu-PC\chandu <chandudammalapati@gmail.com>
2017-07-23 17:41:02 -0400

    creating a sample recipeList component and recipe.modal component to create the structure of recipe

--- src/app/recipes/recipeList/recipeList.component.ts
+++ src/app/recipes/recipeList/recipeList.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 @Component({
     selector:'app-recipeList',
     templateUrl: 'recipeList.component.html'
 })
 export class recipeListComponent implements OnInit {
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe('Egg Sandwich','this is to boil eggs and ','http://tritontimes.com/wp-content/uploads/2015/11/tips.jpg')
+    ];
     constructor(){
 
     }

65921778805005d8e4f31ece17d30ffe8dcc6c12
chandu-PC\chandu <chandudammalapati@gmail.com>
2017-07-23 22:13:27 -0400

    shoppingList component adding sample items to the modal

--- src/app/shoppingList/shoppingList.component.ts
+++ src/app/shoppingList/shoppingList.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.modal';
 @Component({
     selector: 'app-shoppingList',
     templateUrl: 'shoppingList.component.html',
@@ -7,8 +7,11 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class shoppingListComponent implements OnInit {
-    ingredients = [];
-
+    ingredients: Ingredient[] = [
+        new Ingredient('apples', 5),
+        new Ingredient('tomatoes', 10)
+    ];
+    
     constructor(){}
     
     ngOnInit(){

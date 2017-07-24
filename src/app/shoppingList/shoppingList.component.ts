745e8ebdddbe50011f18e135965a5a0e6f491178
chandu-PC\chandu <chandudammalapati@gmail.com>
2017-07-23 21:30:08 -0400

    ShopppingList component changes

--- src/app/shoppingList/shoppingList.component.ts
+++ src/app/shoppingList/shoppingList.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-shoppingList',
-    templateUrl: 'shoppingList.component.html'
+    templateUrl: 'shoppingList.component.html',
+    styleUrls: ['shoppingList.component.css']
 })
 
-export class shoppingListComponent{
+export class shoppingListComponent implements OnInit {
+    ingredients = [];
 
+    constructor(){}
+    
+    ngOnInit(){
+
+    }
 }
